45caaf75398a7432e95544ce2e1dbcca34cc4182
BrutalikCZ <tomas.brezina2006@gmail.com>
2024-05-30 10:52:52 +0200

    Funkční api mezi SQL databází a kompem

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,4 +1,13 @@
+import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, flash
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Heslo123",
+    database="pracovniTest"
+)
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -8,9 +17,21 @@ def handle_form():
     if request.method == 'POST':
         name = request.form.get('name')
         password = request.form.get('password')
-        # Zde můžete přidat další logiku pro zpracování dat, například ověření uživatele
-        flash(f'Přihlášen jako {name}', 'success')
-        return redirect(url_for('handle_form'))
+        
+        cursor = mydb.cursor(dictionary=True)
+        query = "SELECT * FROM user WHERE username = %s AND password = %s"
+        cursor.execute(query, (name, password))
+        user = cursor.fetchone()
+        
+        if user:
+            # Pokud jsou přihlašovací údaje správné, přesměrujeme uživatele na jiný odkaz
+            return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        else:
+            # Pokud jsou přihlašovací údaje špatné, zobrazíme chybu
+            flash('Nesprávné přihlašovací údaje', 'error')
+        
+        cursor.close()
+        
     return render_template('index.html')
 
 if __name__ == '__main__':
