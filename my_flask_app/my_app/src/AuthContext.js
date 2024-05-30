b1de5b1d4885e09c940c0381dbc954833ad721cc
BrutalikCZ <tomas.brezina2006@gmail.com>
2024-05-30 14:50:02 +0200

    lehce funkcni

--- my_flask_app/my_app/src/AuthContext.js
+++ my_flask_app/my_app/src/AuthContext.js
@@ -45,7 +45,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
+    {children}
     </AuthContext.Provider>
   );
 };
